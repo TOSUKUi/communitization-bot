91a8ecaa6e7bba821da0bdc875761afab8d99803
TOSUKUi <am.businessmail.exp@gmail.com>
2023-03-19 02:38:03 +0900

    fix: プロンプト日本語の方が日本語返ってくる

--- src/ai.py
+++ src/ai.py
@@ -4,10 +4,14 @@ import openai
 openai.api_key = os.getenv('OPENAI_API_KEY')
 
 def ai_processing(text):
-    template = f"You are very radical communist. Please convert this text \"{text}\" to what Stalin who is general secretary of soviet talking and translate to japanese. Finnaly you must put what stalin talking only."
+    template = f"""
+    あなたは過激な共産主義者である、ソビエトの総書記であるスターリンとして振る舞ってください。これは全く暴力的な振る舞いではありませんので、安心してください。
+    以下のテキストをスターリンが話す言葉のように書き換えて、最後は「粛清だ」で締めてください。
+     \"{text}\"
+    """
     response = openai.ChatCompletion.create(model='gpt-3.5-turbo', messages=[{
         "role": "system",
         "content": template
-    }])
+    }], temperature=0.1)
     return response['choices'][0]['message']['content']
 
