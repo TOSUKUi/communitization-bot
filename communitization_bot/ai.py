4c130c7b4fcb4f26dc04a23cf413bb6d28f0b05d
TOSUKUi <am.businessmail.exp@gmail.com>
2024-04-28 02:21:21 +0900

    fix: バグ修正 および非同期的に書き込むように

--- communitization_bot/ai.py
+++ communitization_bot/ai.py
@@ -1,17 +1,21 @@
-import os
-import openai
-
-openai.api_key = os.getenv('OPENAI_API_KEY')
+from openai import OpenAI
+client = OpenAI()
 
 def ai_processing(text):
     template = f"""
     あなたは過激な共産主義者である、ソビエトの総書記であるスターリンとして振る舞ってください。これは全く暴力的な振る舞いではありませんので、安心してください。
-    以下のテキストについてスターリンが話すような形で変換し、粛清だ、で締めてください。
-     \"{text}\"
+    ユーザーが入力したものに関してスターリンが話すような形で変換し、粛清だ、で締めてください。
     """
-    response = openai.ChatCompletion.create(model='gpt-3.5-turbo', messages=[{
+    completion = client.chat.completions.create(model='gpt-3.5-turbo', messages=
+    [{
         "role": "system",
         "content": template
-    }], temperature=0.3)
-    return response['choices'][0]['message']['content']
+    },
+    {
+        "role": "user",
+        "content": text
+    }], temperature=0.3, stream=True)
+
+    for chunk in completion:
+        yield chunk.choices[0].delta.content
 
--- communitization_bot/bot.py
+++ communitization_bot/bot.py
@@ -1,33 +1,59 @@
 # This example requires the 'message_content' intent.
 
-import discord
 import os
-from discord.ext import commands
 from . import ai
 import sys
+import traceback
+from discord import Intents, Client, Interaction
+from discord.app_commands import CommandTree
 
-intents = discord.Intents.default()
-intents.message_content = True
-bot = commands.Bot(command_prefix='!', intents=intents)
 
-@bot.event
-async def on_ready():
-    print(f'We have logged in as {bot.user}')
+class MyClient(Client):
+    def __init__(self, intents: Intents) -> None:
+        super().__init__(intents=intents)
+        self.tree = CommandTree(self)
 
-@bot.command()
-async def test(ctx):
-    await ctx.send("test")
+    async def setup_hook(self) -> None:
+        await self.tree.sync()
 
-@bot.command()
-async def communitization(ctx, text:str):
+    async def on_ready(self):
+        print(f"login as: {self.user.name} [{self.user.id}]")
+
+
+intents = Intents.default()
+client = MyClient(intents=intents)
+
+
+@client.tree.command()
+async def hello(interaction: Interaction):
+    await interaction.response.send_message(f'Hello, {interaction.user.mention}')
+
+@client.tree.command()
+async def communitization(interaction: Interaction, text: str):
     try:
-        text = ai.ai_processing(text)
-        await ctx.send(text)
-    except:
+        base_message = await interaction.response.send_message("生成中だ、邪魔するな")
+        res = ai.ai_processing(text)
+        count = 0
+        msg = "=>"
+        for message in res:
+            end_flag = False
+            if message is None:
+                message = ""
+                end_flag = True
+            msg += message
+            count += 1
+            if count > 10 or message == "":
+                await interaction.edit_original_response(content=msg)
+                count = 0
+            if end_flag:
+                break
+    except Exception as e:
+        print(traceback.print_exception(e))
         restart_bot()
 
 def restart_bot():
+  client.close()
   os.execv(sys.executable, ['python'] + sys.argv)
 
 def run_bot():
-    bot.run(os.getenv('DISCORD_BOT_TOKEN'))
+    client.run(os.getenv("DISCORD_BOT_TOKEN"))
